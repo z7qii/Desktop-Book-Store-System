2344b034820f8ca57f1a60db1c27db063912f126
z7qii <140903003+z7qii@users.noreply.github.com>
2024-05-06 21:08:08 +0800

    Update Library.java

--- soft_assignment-part3/Library.java
+++ soft_assignment-part3/Library.java
@@ -7,79 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
-    
-    // public static List<Book2> getRecnetlyAded(){
-
-    //     List <Book2> books = new ArrayList<>();
-    //     Connection connection = null;
-    //     PreparedStatement preparedStatement = null;
-    //     ResultSet resultSet = null;
-
-    //     try{
-    //         connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Assignment_1", "root", "uvhrkh12");
-    //         preparedStatement = connection.prepareStatement("SELECT * FROM  recentlyAdded");
-    //         resultSet = preparedStatement.executeQuery();
-
-    //         if(resultSet.isBeforeFirst()){
-    //             while(resultSet.next()){
-    //                 Book2 book = new Book2(resultSet.getString("bookID"), resultSet.getString("bookName"), Double.parseDouble(resultSet.getString("bookID")) , resultSet.getString("bookCategory"), resultSet.getString("bookAuthor"));
-    //                 books.add(book);
-    //             }
-                
-    //         }
-
-            
-    //     }catch(SQLException e){
-
-    //         e.printStackTrace();
-
-    //     } finally{
-
-    //         try { if (resultSet != null) resultSet.close(); } catch (SQLException e) {e.printStackTrace();};
-    //         try { if (preparedStatement != null) preparedStatement.close(); } catch (SQLException e) {e.printStackTrace();};
-    //         try { if (connection != null) connection.close(); } catch (SQLException e) {e.printStackTrace();};
-
-    //     }
-
-    //     return books;
-    // }
-
-
-    // public static List<Book2> getRecomnded(){
-
-    //     List <Book2> books = new ArrayList<>();
-    //     Connection connection = null;
-    //     PreparedStatement preparedStatement = null;
-    //     ResultSet resultSet = null;
-
-    //     try{
-    //         connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Assignment_1", "root", "uvhrkh12");
-    //         preparedStatement = connection.prepareStatement("SELECT * FROM  recomended");
-    //         resultSet = preparedStatement.executeQuery();
-
-    //         if(resultSet.isBeforeFirst()){
-    //             while(resultSet.next()){
-    //                 Book2 book = new Book2(resultSet.getString("bookID"), resultSet.getString("bookName"), Double.parseDouble(resultSet.getString("bookID")) , resultSet.getString("bookCategory"), resultSet.getString("bookAuthor"));
-    //                 books.add(book);
-    //             }
-                
-    //         }
-
-            
-    //     }catch(SQLException e){
-
-    //         e.printStackTrace();
-
-    //     } finally{
-
-    //         try { if (resultSet != null) resultSet.close(); } catch (SQLException e) {e.printStackTrace();};
-    //         try { if (preparedStatement != null) preparedStatement.close(); } catch (SQLException e) {e.printStackTrace();};
-    //         try { if (connection != null) connection.close(); } catch (SQLException e) {e.printStackTrace();};
-
-    //     }
-
-    //     return books;
-    // }
 
     public static String getBookImage(String bookId){
         String bookImage ;
@@ -88,7 +15,7 @@ public class Library {
         ResultSet resultSet = null;
 
         try{
-            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/softWare_assignment", "root", "uvhrkh12");
+            connection = DriverManager.getConnection("URL");
             preparedStatement = connection.prepareStatement("SELECT bookImage FROM  books WHERE bookID = ?");
             preparedStatement.setString(1, bookId);
             resultSet = preparedStatement.executeQuery();
@@ -129,7 +56,7 @@ public class Library {
         ResultSet resultSet = null;
 
         try{
-            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/softWare_assignment", "root", "uvhrkh12");
+            connection = DriverManager.getConnection("URL");
             preparedStatement = connection.prepareStatement("SELECT * FROM  books");
             resultSet = preparedStatement.executeQuery();
 
@@ -167,7 +94,7 @@ public class Library {
         ResultSet resultSet = null;
 
         try{
-            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/softWare_assignment", "root", "uvhrkh12");
+            connection = DriverManager.getConnection("URL");
             preparedStatement = connection.prepareStatement("SELECT * FROM  books WHERE bookID = ?");
             preparedStatement.setString(1, bookID);
             resultSet = preparedStatement.executeQuery();
